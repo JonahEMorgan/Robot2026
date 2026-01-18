c55a5f83ea47e247321b7618335e47a3b38943e6
Jonah Morgan <jlamorgan5@gmail.com>
2026-01-17 21:26:33 -0500

    Document code

--- src/main/java/frc/robot/subsystems/BasicMotorSubsystem.java
+++ src/main/java/frc/robot/subsystems/BasicMotorSubsystem.java
@@ -7,51 +7,110 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
+/**
+ * An abstract subsystem which contains a single motor that can be either moved
+ * for a specific duration or using a controller button.
+ * 
+ * See {@link Transport}, {@link Intake}, {@link Shooter}, or {@link Climber}
+ * for examples of how to use this class.
+ */
 public abstract class BasicMotorSubsystem extends SubsystemBase {
+	/**
+	 * The motor controller allows us to change the speed of the motor
+	 */
 	private final SparkMax m_motor;
+	/**
+	 * The default speed of the motor is stored for use in commands
+	 */
 	private final double m_speed;
 
+	/**
+	 * Creates a new subsystem using the default speed and CAN id provided by the
+	 * implementing class.
+	 */
 	public BasicMotorSubsystem() {
 		m_motor = new SparkMax(getMotorId(), MotorType.kBrushless);
 		m_speed = getDefaultSpeed();
 	}
 
+	/**
+	 * Gets the CAN id for the motor controller.
+	 *
+	 * @return CAN id
+	 */
 	protected abstract int getMotorId();
 
+	/**
+	 * Gets the default speed for the motor.
+	 *
+	 * @return default speed
+	 */
 	protected abstract double getDefaultSpeed();
 
+	/**
+	 * Creates a new command that moves the motor at a fixed speed for the specified
+	 * duration.
+	 *
+	 * @param speed motor speed
+	 * @param time duration in seconds
+	 * @return command
+	 */
 	public Command moveForTime(double speed, double time) {
 		return startEnd(
-				() -> m_motor.set(speed),
-				() -> m_motor.stopMotor())
-						.withTimeout(time)
+				() -> m_motor.set(speed), // Set the motor speed to the specified value
+				() -> m_motor.stopMotor()) // Stop the motor when the command ends
+						.withTimeout(time) // End the command after the specified duration
 						.withName("moveForTime");
 	}
 
+	/**
+	 * Creates a new command that moves the motor at the default speed for the
+	 * specified duration.
+	 *
+	 * @param time duration in seconds
+	 * @return command
+	 */
 	public Command moveForTime(double time) {
 		return moveForTime(m_speed, time);
 	}
 
+	/**
+	 * Creates a new command that moves the motor at a fixed speed when buttons are
+	 * pressed.
+	 *
+	 * @param speed motor speed
+	 * @param forward button which causes forward movement
+	 * @param back button which causes backward movement
+	 * @return command
+	 */
 	public Command moveWithTrigger(double speed, Trigger forward, Trigger back) {
 		return runEnd(
 				() -> {
-					double sum = 0;
+					double sum = 0; // Don't move the motor if no triggers are active.
 					if (forward != null) {
 						if (forward.getAsBoolean()) {
-							sum += speed;
+							sum += speed; // Increase the speed when the forward trigger is active.
 						}
 					}
 					if (back != null) {
 						if (back.getAsBoolean()) {
-							sum -= speed;
+							sum -= speed; // Decrease the speed when the backward trigger is active.
 						}
 					}
-					m_motor.set(sum);
+					m_motor.set(sum); // Set the motor speed based on the active triggers.
 				},
-				() -> m_motor.stopMotor())
+				() -> m_motor.stopMotor()) // Stop the motor when the command ends
 						.withName("moveWithTrigger");
 	}
 
+	/**
+	 * Creates a new command that moves the motor at the default speed when buttons
+	 * are pressed.
+	 *
+	 * @param forward button which causes forward movement
+	 * @param back button which causes backward movement
+	 * @return command
+	 */
 	public Command moveWithTrigger(Trigger forward, Trigger back) {
 		return moveWithTrigger(m_speed, forward, back);
 	}
--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -13,14 +13,43 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Compliance;
 import frc.robot.Constants.Subsystems.TurretConstants;
 
+/**
+ * A subsystem which has the ability to change both yaw and pitch in order to
+ * adjust the direction fuel will exit the shooter.
+ */
 public class Turret extends SubsystemBase {
+	/**
+	 * The yaw motor allows the mechanism to pivot left and right.
+	 */
 	private final SparkMax m_yawMotor;
+	/**
+	 * The pitch motor allows the mechanism to pivot up and down.
+	 */
 	private final SparkMax m_pitchMotor;
+	/**
+	 * The through bore encoder on the yaw axis allows us to check the yaw angle.
+	 */
 	private final SparkAbsoluteEncoder m_yawEncoder;
+	/**
+	 * The through bore encoder on the pitch axis allows us to check the pitch
+	 * angle.
+	 */
 	private final SparkAbsoluteEncoder m_pitchEncoder;
+	/**
+	 * The yaw PID controller is used to aim the turret in a specific yaw direction
+	 * in a closed feedback loop.
+	 */
 	private final PIDController m_yawPid;
+	/**
+	 * The pitch PID controller is used to aim the turret in a specific pitch
+	 * direction in a closed feedback loop.
+	 */
 	private final PIDController m_pitchPid;
 
+	/**
+	 * Creates a new subsystem using the motor id's from the turret constants, the
+	 * corresponding pid constants from the turret constants, and a proper name.
+	 */
 	public Turret() {
 		setName("Turret subsystem");
 		{
@@ -39,51 +68,90 @@ public class Turret extends SubsystemBase {
 		}
 	}
 
+	/**
+	 * Wraps a {@link DoubleSupplier} in a {@link DoubleSupplier} in order to apply
+	 * a deadzone on the signal.
+	 * 
+	 * @param supplier original signal
+	 * @param deadzone size of the deadzone
+	 * @return modified signal
+	 */
 	public DoubleSupplier addDeadzone(DoubleSupplier supplier, double deadzone) {
 		return () -> MathUtil.applyDeadband(supplier.getAsDouble(), deadzone);
 	}
 
+	/**
+	 * Creates a new command which aims the turret by moving in the direction of a
+	 * joystick.
+	 * 
+	 * @param yaw yaw velocity signal from joystick
+	 * @param pitch pitch velocity signal from joystick
+	 * @return command
+	 */
 	public Command aimWithJoystick(DoubleSupplier yaw, DoubleSupplier pitch) {
 		double deadzone = Compliance.ensure(TurretConstants.class, "kDeadzone");
-		DoubleSupplier yawValue = addDeadzone(yaw, deadzone);
-		DoubleSupplier pitchValue = addDeadzone(pitch, deadzone);
+		DoubleSupplier yawValue = addDeadzone(yaw, deadzone); // Add a deadzone to the yaw signal.
+		DoubleSupplier pitchValue = addDeadzone(pitch, deadzone); // Add a deadzone to the pitch signal.
 		return runEnd(
 				() -> {
-					m_yawMotor.set(yawValue.getAsDouble());
-					m_pitchMotor.set(pitchValue.getAsDouble());
+					m_yawMotor.set(yawValue.getAsDouble()); // Move the yaw motor in the yaw direction.
+					m_pitchMotor.set(pitchValue.getAsDouble()); // Move the pitch motor in the pitch direction.
 				},
 				() -> {
-					m_yawMotor.stopMotor();
+					m_yawMotor.stopMotor(); // Stop both motors when the command ends.
 					m_pitchMotor.stopMotor();
 				})
 						.withName("aimWithJoystick");
 	}
 
+	/**
+	 * Creates a new command which aims the turret at a specific fixed angle.
+	 * 
+	 * @param yaw yaw angle in degrees
+	 * @param pitch pitch angle in degrees
+	 * @return command
+	 */
 	public Command aimAtAngle(double yaw, double pitch) {
 		return aimAtAngle(
-				() -> yaw,
-				() -> pitch).beforeStarting(
-						() -> {
-							m_yawPid.setSetpoint(yaw);
-							m_pitchPid.setSetpoint(pitch);
-						}).until(
-								() -> m_yawPid.atSetpoint() && m_pitchPid.atSetpoint());
+				() -> yaw, // Always aim in the fixed yaw direction.
+				() -> pitch) // Always aim in the fixed pitch direction.
+						.beforeStarting(
+								() -> {
+									m_yawPid.setSetpoint(yaw); // Set both the pitch and yaw setpoints when the command
+																// is initialized.
+									m_pitchPid.setSetpoint(pitch);
+								})
+						.until(
+								() -> m_yawPid.atSetpoint() && m_pitchPid.atSetpoint()); // End the command when both
+																							// the target pitch and
+																							// target yaw angles have
+																							// been reached.
 	}
 
+	/**
+	 * Creates a new command which aims the turret at a constantly changing angle,
+	 * likely derived from vision data.
+	 * 
+	 * @param yaw yaw angle signal in degrees
+	 * @param pitch pitch angle signal in degrees
+	 * @return command
+	 */
 	public Command aimAtAngle(DoubleSupplier yaw, DoubleSupplier pitch) {
 		return runEnd(
 				() -> {
 					m_yawMotor.set(
-							m_yawPid.calculate(
-									m_yawEncoder.getPosition(),
-									yaw.getAsDouble()));
+							m_yawPid.calculate( // Use the yaw PID controller to calculate the yaw speed.
+									m_yawEncoder.getPosition(), // Get the current yaw angle.
+									yaw.getAsDouble())); // Specify the yaw target to be the value of the yaw angle
+															// signal.
 					m_pitchMotor.set(
-							m_pitchPid.calculate(
-									m_pitchEncoder.getPosition(),
-									pitch.getAsDouble()));
+							m_pitchPid.calculate( // Use the pitch PID controller to calculate the pitch speed.
+									m_pitchEncoder.getPosition(), // Get the current pitch angle.
+									pitch.getAsDouble())); // Specify the pitch target to be the value of the pitch
+															// angle signal.
 				},
 				() -> {
-					m_yawMotor.stopMotor();
+					m_yawMotor.stopMotor(); // Stop both motors when the command ends.
 					m_pitchMotor.stopMotor();
 				})
 						.withName("aimAtAngle");
